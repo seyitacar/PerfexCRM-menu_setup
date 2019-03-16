ddd333efe71ddbdb418c6152d39b03d317446753
Seyit Acar <seyitacar23@gmail.com>
2019-03-16 20:43:13 +0300

    Update menu_setup_lang.php

--- turkish/menu_setup_lang.php
+++ turkish/menu_setup_lang.php
@@ -1,9 +1,9 @@
 <?php
 
 # Version 2.3.0
-$lang['menu_builder']        = 'Menu Kurulumu';
-$lang['main_menu']           = 'Ana Menu';
-$lang['setup_menu']          = 'Menu Kurulumu';
-$lang['utilities_menu_icon'] = 'Icon';
-$lang['active_menu_items']   = 'Aktif Menu Nesneleri';
+$lang['menu_builder']        = 'Menü Oluşturucu';
+$lang['main_menu']           = 'Ana Menü';
+$lang['setup_menu']          = 'Menü Oluşturucu';
+$lang['utilities_menu_icon'] = 'Simge';
+$lang['active_menu_items']   = 'Aktif Menü Ögeleri';
 $lang['utilities_menu_save'] = 'Menüyü Kaydet';
